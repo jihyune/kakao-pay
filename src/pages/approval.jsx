dc05cc016b7a15805cc59f07cc8f158c1daccee6
syrius <djlee1031@snu.ac.kr>
2023-11-25 13:39:04 +0900

    skeleton code

--- src/pages/approval.jsx
+++ src/pages/approval.jsx
@@ -1,51 +1,5 @@
-import { useState } from "react";
-import { dummyData } from "../constant";
-import { approvePayAxios } from "../api/axios";
 const ApprovalPage = () => {
-  const pgToken = new URL(window.location.href).searchParams.get("pg_token");
-
-  const [approvedData, approve] = useState();
-  const approvePay = async () => {
-    const tid = localStorage.getItem("tid");
-    const partner_user_id = localStorage.getItem("partner_user_id");
-
-    if (!tid || !partner_user_id) return;
-    if (!pgToken) return;
-
-    const approvePayData = {
-      ...dummyData,
-      tid,
-      partner_user_id: partner_user_id,
-      pg_token: pgToken,
-    };
-    const returnedData = await approvePayAxios(approvePayData);
-    approve(returnedData);
-  };
-
-  return (
-    <>
-      {!approvedData ? (
-        <button onClick={approvePay}>결제완료</button>
-      ) : (
-        <div>
-          <p>결제가 완료되었습니다.</p>
-          <p>
-            결제완료시각:
-            {approvedData.approved_at.toString()}
-          </p>
-          <p>
-            상품명:
-            {approvedData.item_name}
-          </p>
-          <p>
-            결제금액:
-            {approvedData.amount.total}
-          </p>
-          <p>결제고유번호:{approvedData.tid}</p>
-        </div>
-      )}
-    </>
-  );
+    return <div>approval</div>;
 };
 
 export default ApprovalPage;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,34 +1,20 @@
 import { useEffect, useState } from "react";
-import { readyPayAxios } from "../api/axios";
 import { Form } from "../components/form";
 
 const HomePage = () => {
   const [readyData, setReadyData] = useState();
-
-  const readyPay = async (readyPayData) => {
-    const returnedData = await readyPayAxios(readyPayData);
-    localStorage.setItem("partner_user_id", readyPayData.partner_user_id);
-    setReadyData(returnedData);
-  };
-
-  useEffect(() => {
-    if (!readyData) return;
-    console.log(readyData);
-    window.open(readyData.next_redirect_pc_url);
-    localStorage.setItem("tid", readyData.tid);
-  }, [readyData]);
 
   return (
     <div>
       {readyData ? (
         <div>
           <p>결제 진행중입니다 . . .</p>
-          <button onClick={() => window.open(readyData.next_redirect_pc_url)}>
+          <button onClick={() => window.open()}>
             결제 이어서 하기
           </button>
         </div>
       ) : (
-        <Form readyData={readyData} readyPay={readyPay} />
+        <Form readyData={readyData} readyPay={setReadyData} />
       )}
     </div>
   );
